1b6a6c6812a3df5c61b6a6539a896068dcf9707a
TheintTheintSH <theinttheintsoehtet.edka@gmail.com>
2025-03-18 17:50:52 +0630

    0Auth google login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
 function App() {
-  return <h1>Learn With Podcasts</h1>;
+  const [user, setUser] = useState({});
+  const handleCallBack = (res) => {
+    let user = jwtDecode(res.credential);
+    setUser(user);
+  };
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id:
+        "574602703280-5ek37ebton0o86irqtfhcv9r2ef0qqlv.apps.googleusercontent.com",
+      callback: handleCallBack,
+    });
+    google.accounts.id.renderButton(document.getElementById("SignIn"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-1xl mt-5 py-2 text-center">Learn With Podcasts</h1>
+      <div id="SignIn"></div>
+      {user && (
+        <>
+          <img src={user.picture} alt="User Profile" />
+          <h3>{user.name}</h3>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
